25d4bef14f10104e302b738d53353bd907feb117
Towich <towichf@gmail.com>
2023-05-01 21:40:51 +0300

    feat: connect Jsoup

--- app/src/main/java/com/example/parsingcryptocurrency/MainActivity.java
+++ app/src/main/java/com/example/parsingcryptocurrency/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Document doc;
+    Runnable runnable;
+    Thread secThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init(){
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                getWeb();
+            }
+        };
+
+        secThread = new Thread(runnable);
+        secThread.start();
+    }
+
+    private void getWeb(){
+        try {
+            doc = Jsoup.connect("https://coinmarketcap.com/").get();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
